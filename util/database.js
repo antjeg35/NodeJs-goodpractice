3422071a6dc3c91d37ee9be01349201fdab3603c
Antoine <ajegou35@gmail.com>
2020-02-29 16:36:48 +0100

    1st approach SQL

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Pa$$w0rd'
+});
+
+module.exports = pool.promise();
