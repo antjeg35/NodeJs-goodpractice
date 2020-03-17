49f05c034570a8dc60219c2d7b455c62f067eb11
Antoine <ajegou35@gmail.com>
2020-03-17 09:07:39 +0100

    initialize mongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'Pa$$w0rd', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+  'mongodb+srv://dbAntoine:DwFny3r34m9nl43g@cluster0-9quvw.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(client => {
+    console.log('Connected');
+    _db = client.db();
+    callback();
+  })
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
+}
+
+const getDb = () => {
+  if(_db){
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
